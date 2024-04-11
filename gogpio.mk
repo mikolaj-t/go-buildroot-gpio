b898857b58ddae0a2c947685084391da6a4f9767
mikolaj-t <mikolaj.t@outlook.com>
2024-04-12 01:44:33 +0200

    bump version

--- gogpio.mk
+++ gogpio.mk
@@ -3,7 +3,7 @@
 # gogpio
 #
 ################################################################################
-GOGPIO_VERSION = 0.16
+GOGPIO_VERSION = 0.17
 GOGPIO_SITE = $(call github,mikolaj-t,go-buildroot-gpio,$(GOGPIO_VERSION))
 
 $(eval $(golang-package))
